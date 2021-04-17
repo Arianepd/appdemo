303a276273a2d4dbb5f7691591bb52d2d2eb0d08
Ariane Coelho <arianeclh09@gmail.com>
2021-04-16 21:28:37 -0300

    Inclusão dos arquivos iniciais ao repo

---
+++ src/main/java/com/zrtech/appdemo/AppdemoApplication.java
@@ -0,0 +1,14 @@
+package com.zrtech.appdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdemoApplication {
+
+	public static void main(String[] args) {
+
+		git SpringApplication.run(AppdemoApplication.class, args);
+	}
+
+}
